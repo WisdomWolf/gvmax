5ba7d4732d5c47f1898685b4d12b129641ebfa5d
haninaguib <hani.naguib@mellmo.com>
2014-02-24 23:25:50 -0800

    Last minute changes before initial deployment.

--- com.gvmax.server/src/main/java/com/gvmax/server/XMPPReceiver.java
+++ com.gvmax.server/src/main/java/com/gvmax/server/XMPPReceiver.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     Hani Naguib - initial API and implementation
  ******************************************************************************/
@@ -45,6 +45,7 @@ public class XMPPReceiver implements QueueProcessor<XMPPAction> {
     public XMPPReceiver(UserDAO userDAO, GVMaxRelay relay) {
         this.userDAO = userDAO;
         this.relay = relay;
+        logger.info("ready to process emails.");
     }
 
     @Override
